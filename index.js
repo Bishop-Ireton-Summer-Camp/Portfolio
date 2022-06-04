2db1b343929eb71df7d992913ac8d7ab87fab69d
Terri Kelly <kellyt@bishopireton.org>
2022-06-04 13:59:28 -0400

    Start images and table

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60))/ 1000);
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer").innHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     // add text if (distance < 0)
 
