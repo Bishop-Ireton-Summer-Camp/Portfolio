9b43140eaa90b243118a4471aeb0054eae10ec25
Terri Kelly <kellyt@bishopireton.org>
2022-06-07 17:09:33 -0400

    Converter is working

--- converter.js
+++ converter.js
@@ -1,3 +1,72 @@
+function convert(){
+    var fromTypeIndex = document.getElementById("fromType").selectedIndex;
+    var types = document.getElementById("fromType").options;
+    var fromType = types[fromTypeIndex].text;
+
+    var toTypeIndex = document.getElementById("toType").selectedIndex;
+    var toType = types[toTypeIndex].text;
+
+    var fromString = document.getElementById("from").innerHTML;
+    var to = document.getElementById("to");
+
+    switch(fromType) {
+        case "Binary":
+            switch(toType) {
+                case "Binary":
+                      to.innerHTML=fromString; // set the to to from as base is the same
+                    break;
+                case "Decimal":
+                      to.innerHTML=parseInt(fromString, 2);
+                    break;
+                case "Hexadecimal":
+                      to.innerHTML=parseInt(fromString, 2).toString(16);
+                    break;
+             }
+            break;
+
+        case "Decimal":
+            var from = parseInt(fromString, 10);
+            switch(toType) {
+                case "Binary":
+                      to.innerHTML=from.toString(2);
+                    break;
+                case "Decimal":
+                    to.innerHTML=from;
+                    break;
+                case "Hexadecimal":
+                    to.innerHTML=from.toString(16);
+                    break;
+             }
+            break;
+        case "Hexadecimal":
+            switch(toType) {
+                case "Binary":
+                    to.innerHTML=parseInt(fromString, 16).toString(2);
+                    break;
+                case "Decimal":
+                      to.innerHTML=parseInt(fromString, 16);
+                    break;
+                case "Hexadecimal":
+                    to.innerHTML=fromString; // set the to to from as base is the same
+                     
+                    break;
+             }
+            break;
+    }  
+}
+
+// Add the digit of the button to the end of the from
+function addDigit(button) {
+    var from = document.getElementById("from");
+    if (from.innerHTML == "0")
+        from.innerHTML=button.innerHTML;    
+    else 
+       from.innerHTML= from.innerHTML + button.innerHTML;
+}
+
+// Runs when user selects an item from the From Drop Down box
+// makes the correct number keypad show and hides the others
+
 function changeFrom(event) {
     var newType = this.options[this.selectedIndex].text;
     var binaryButtons = document.getElementById("binaryButtons");
