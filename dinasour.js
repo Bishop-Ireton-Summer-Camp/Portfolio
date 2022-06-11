c1ef8fa9d1856c7e046037dcd19c6166a54dd16f
Terri Kelly <kellyt@bishopireton.org>
2022-06-10 21:52:37 -0400

    Added jump capability

--- dinasour.js
+++ dinasour.js
@@ -1,18 +1,50 @@
 var id = null;
 
 function moveEnemy() {
-    var skater = document.getElementById("enemy");
     var pos = screen.width;
     clearInterval(id);
     id = setInterval(frame, 5);
     function frame() {
         if (pos <= 0) {
             pos = screen.width;
-            //clearInterval(id);
+            //    var enemy = document.getElementById("enemy");
+            clearInterval(id);
         } else {
             pos--;
-            //skater.style.top = pos + 'px';
-            skater.style.left = pos + 'px';
+            enemy.style.left = pos + 'px';
         }
+    }
+}
+
+var loc = 0;
+var initialSkaterLocation = 0;
+
+function setup() {
+    var skater = document.getElementById("skater");
+    loc = skater.offsetTop - skater.scrollTop;
+    initialSkaterLocation = loc; // save to reset skater after jump
+    document.addEventListener('keydown', (event)=>{
+      //  var keyValue = event.key;
+      //  var keyCode = event.code;
+      //  console.log("key value: " + keyValue);
+      //  console.log("code value: " + keyCode);
+        jump();
+    }, false);
+}
+
+var jumpInterval = null;
+
+function jump() {
+    var skater = document.getElementById("skater");
+    loc -= 200;
+    console.log(loc);
+    skater.style.top = loc + 'px';
+
+    id = setInterval(fall, 500);
+    function fall() {
+        
+        skater.style.top = initialSkaterLocation + 'px';
+        loc = initialSkaterLocation;
+        clearInterval(id);
     }
 }
