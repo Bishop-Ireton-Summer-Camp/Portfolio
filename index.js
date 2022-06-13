3249a93d58b82ff0743cfe95d447a97b417f4a61
Terri Kelly <kellyt@bishopireton.org>
2022-06-12 22:28:31 -0400

    Adding more complicated CSS ... organizing changes into table

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var countDownDate = new Date("Dec 31, 2022 7:00:00").getTime();
 
 //
 var x = setInterval(function() {
-    var now = new Date().getTime(); // Get Today's Date and Time
+    var now = new Date().getTime(); // Get (Today's Date and Time
     var distance = countDownDate - now;
 
     var days = Math.floor(distance / (1000 * 60 * 60 *24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60))/ 1000);
+    var minutes = Math.floor((distance % (1000 * 60 * 60))/ (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
